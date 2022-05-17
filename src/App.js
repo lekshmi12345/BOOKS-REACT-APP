43cb631a7d67c6b78e98ee04471748ff27c8fbfb
lekshmi12345 <lekshmisnair213@gmail>
2022-05-17 21:40:57 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+
+
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
 import Addstudent from './components/Addstudent';
 import Searchstudent from './components/Searchstudent';
+import Navbar from './components/Navbar';
+
 
 function App() {
   return (
     <>
-    <Searchstudent/>
-    <Addstudent/>
-    
-    
+    <div>
+      <Navbar/>
+     <BrowserRouter>
+     <Routes>
+<Route path="/" exact element={<Addstudent/>}/>
+<Route path="/search" exact element={<Searchstudent/>}/>
+
+     </Routes>
+     </BrowserRouter>
+    </div>
     </>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+
+
+const Navbar = () => {
+  return (
+      <>
+    <div>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-success">
+  <div class="container-fluid">
+    <Link class="navbar-brand" to="/">BOOK</Link>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <Link class="nav-link active" to="/">Addstudent</Link>
+        </li>
+        <li class="nav-item">
+          <Link class="nav-link" to="/search">Searchstudent</Link>
+        </li>
+       
+       
+      </ul>
+    </div>
+  </div>
+</nav>
+    </div>
+    
+    </>
+    
+    
+  )
+}
+
+export default Navbar
