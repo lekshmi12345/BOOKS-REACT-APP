8f0720d0d14ca6094864f5dac026b133c3e9a9b0
lekshmi12345 <lekshmisnair213@gmail>
2022-05-17 20:17:08 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import Addstudent from './components/Addstudent';
+import Searchstudent from './components/Searchstudent';
 
 function App() {
   return (
     <>
+    <Searchstudent/>
     <Addstudent/>
     
     
---
+++ src/components/Searchstudent.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+
+const Searchstudent = () => {
+  return (
+    <div>
+        <div className="container">
+    <div className="row">
+        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+            <div className="row g-3">
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <label for="" className="form-label">BOOK TITLE:</label>
+                    <input type="text" className="form-control"/>
+                </div>
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <button className="btn btn-success">SEARCH</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+    </div>
+    
+  )
+}
+
+export default Searchstudent
